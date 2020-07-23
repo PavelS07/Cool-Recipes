23fee043c61aff8839214be60c0322b04b293f6e
PavelS07 <62809253+PavelS07@users.noreply.github.com>
2020-07-23 03:09:31 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
   const iconSearch  = $('.search-icon');
   const blockSearch  = $('.search-block'); 
   const bClose = $('.close'); 
+  const filterButton = $('.submit-filter');
+  const bCloseModal = $('.modal-close');
+  const bChooseIngredients = $('.button-choose-ingredients');
+  const bCloseModalChooseIngredients = $('.modal-close-choose-ingredients');
   let width = $(window).width(); 
 
   iconSearch.on("click", function() {
@@ -24,6 +28,21 @@ $(document).ready(function () {
       iconSearch.toggleClass('search-visible');
       blockSearch.removeClass('search-visible');
     }
-
   });
+  filterButton.on("click", function() {
+    $('.modal').toggleClass('is-open-modal');
+  });
+  bCloseModal.on("click", function() {
+    $('.modal').toggleClass('is-open-modal');
+  });
+  bChooseIngredients.on("click", function() {
+    $('.modal-choose-ingredients').toggleClass('is-open-modal');
+  });
+  bCloseModalChooseIngredients.on("click", function() {
+    $('.modal-choose-ingredients').toggleClass('is-open-modal');
+    $('.modal').toggleClass('is-open-modal');
+  });
+  
+
+
 }); 
